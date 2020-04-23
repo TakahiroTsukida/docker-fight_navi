5c77bf6ece653388ce6fa5072a4e5839fd6ca749
”TakahiroTsukida” <la.tsukida@gmail.com>
2020-04-24 02:08:43 +0900

    2020/04/23

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,29 +5,65 @@
     </head>
     <body>
         <div id="app">
-            <nav class="navbar navbar-light navbar-laravel">
-                <div class="container">
-
+            <nav>
+                <div>
                     @guest
-                        <div class="mx-2">
-                            <p class="site-title">
-                                <a href="{{ url('/') }}">
-                                    <img src="{{ asset('image/Fightなび.png') }}" alt="Fightなび">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <div class="header">
+                                <a class="navbar-brand header-img" href="{{ url('/') }}">
+                                    <img src="{{ asset('storage/image/app_images/Fightなび.png') }}" alt="Fightなび">
                                 </a>
-                            </p>
-                        </div>
-                        <div class="nav justify-content-end mx-2">
-                            <a href="{{ route('login') }}" class="btn btn-default rounded-pill mx-2 header-btn">ログイン</a>
-                            <a href="{{ route('register') }}" class="btn btn-primary rounded-pill header-btn">新規登録</a>
-                        </div>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto">
-                            </ul>
-                            <ul class="navbar-nav ml-auto">
-                            </ul>
-                        </div>
+                                <button class="navbar-toggler menu-icon" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                            </div>
+                            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                <div class="navbar-nav clear_box">
+                                      <a class="nav-item active" href="#"><i class="fas fa-search fa-lg"></i><span class="menu-label">ジム検索<span><span class="sr-only">(current)</span></a>
+                                      <a class="nav-item" href="#"><i class="fas fa-edit fa-lg"></i><span class="menu-label">新規レビュー</span></a>
+                                      <a class="nav-item" href="{{ route('login') }}"><i class="fas fa-sign-in-alt fa-lg small"></i><span class="menu-label">ログイン</span></a>
+                                      <a class="nav-item disabled" href="{{ route('register') }}"><i class="fas fa-user-plus fa-lg"></i><span class="menu-label">新規登録</span></a>
+                                </div>
+                            </div>
+                        </nav>
                     @else
-                        <div id="nav-drawer">
+                        <nav class="navbar navbar-light">
+                            <div class="header">
+                                <a class="navbar-brand header-img" href="{{ url('/') }}">
+                                    <img src="{{ asset('storage/image/app_images/Fightなび.png') }}" alt="Fightなび">
+                                </a>
+                                <button class="navbar-toggler menu-icon" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                            </div>
+                            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                <div class="navbar-nav clear_box">
+                                      <a class="nav-item-login active" href="{{ route('user.profile.mypage', ['id' => Auth::user()->id]) }}"><i class="fas fa-user fa-lg default"></i><span class="login-label">マイページ</span><span class="sr-only">(current)</span></a>
+
+                                      @if(Auth::user()->profiles)
+                                          <a class="nav-item-login" href="{{ route('user.profile.edit',['id' => Auth::user()->id]) }}"><i class="fas fa-user-cog fa-lg"></i><span class="login-label">プロフィール編集</span><span class="sr-only">(current)</span></a>
+                                      @else
+                                          <a class="nav-item-login" href="{{ route('user.profile.add',['id' => Auth::user()->id]) }}"><i class="fas fa-user-cog fa-lg"></i><span class="login-label">プロフィール作成</span><span class="sr-only">(current)</span></a>
+                                      @endif
+
+                                      <a class="nav-item-login" href="{{ route('user.search') }}"><i class="fas fa-search fa-lg default"></i><span class="login-label">ジム検索</span><span class="sr-only">(current)</span></a>
+
+                                      <a class="nav-item-login" href="{{ route('user.search') }}"><i class="fas fa-edit fa-lg default"></i><span class="login-label">新規レビュー</span></a>
+                                      <a class="nav-item-login" href="#"><i class="fas fa-envelope fa-lg default"></i><span class="login-label">メールアドレス変更</span></a>
+                                      <a class="nav-item-login disabled" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                   document.getElementById('logout-form').submit();">
+                                             <i class="fas fa-sign-out-alt fa-lg default"></i><span class="login-label">ログアウト</span></a>
+                                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                      @csrf
+                                      </form>
+                                </div>
+                            </div>
+                        </nav>
+
+
+
+
+                        <!-- <div id="nav-drawer">
                             <input id="nav-input" type="checkbox" class="nav-unshown">
                             <label id="nav-open" for="nav-input">
                                 <span></span>
@@ -89,7 +125,7 @@
                             <a class="mx-auto mt-3" href="{{ url('/') }}">
                                 <img src="{{ asset('image/Fightなび.png') }}" alt="Fightなび">
                             </a>
-                        </p>
+                        </p> -->
                     @endguest
 
 
