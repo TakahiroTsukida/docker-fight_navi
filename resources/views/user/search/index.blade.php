f5e5875ece0e1e7b4d4e9d957b681a84ee3424e5
”TakahiroTsukida” <la.tsukida@gmail.com>
2020-05-12 01:18:34 +0900

    評価点の四捨五入を追記

--- resources/views/user/search/index.blade.php
+++ resources/views/user/search/index.blade.php
@@ -217,7 +217,7 @@
                                                   @break
                                               @endswitch
 
-                                              <p class="review-point">{{ $shop->point }}点</p>
+                                              <p class="review-point">{{ round($shop->point, 2) }}点</p>
                                               <p class="review-count"><i class="far fa-comment-alt fa-lg"></i> {{ $shop->reviews_count }}</p>
                                         @else
                                             <p class="review-point"></p>
