78fdafc55cba53d0282daa64bbd46ed176a5472e
”TakahiroTsukida” <la.tsukida@gmail.com>
2020-07-06 01:04:57 +0900

    shopテストデータ再

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,18 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      $this->call([
-          // TypesTableSeeder::class,
-          // UserSeeder::class,
-          // AdminsTableSeeder::class,
-          // ShopsTableSeeder::class,
-          // ReviewsTableSeeder::class,
-          // FavoritesTableSeeder::class,
-          JoinPriceTableSeeder::class,
-          OpenTableSeeder::class,
-          OtherPriceTableSeeder::class,
-          PriceTableSeeder::class,
+      $types = App\Admin\Type::all();
 
-      ]);
+      factory(App\Admin\Shop::class, 100)
+            ->create()
+            ->each(function ($shop) use ($types) {
+                $shop->types()->attach(
+                    $types->random(rand(1,3))->pluck('id')->toArray()
+                );
+            });
+
+
+
+      // $this->call([
+      //     // TypesTableSeeder::class,
+      //     // UserSeeder::class,
+      //     // AdminsTableSeeder::class,
+      //     // ShopsTableSeeder::class,
+      //     // ReviewsTableSeeder::class,
+      //     // FavoritesTableSeeder::class,
+      //     // JoinPriceTableSeeder::class,
+      //     // OpenTableSeeder::class,
+      //     // OtherPriceTableSeeder::class,
+      //     // PriceTableSeeder::class,
+
+
+      // ]);
     }
 }
